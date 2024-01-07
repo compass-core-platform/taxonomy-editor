cade3441752ef75feba9464963db6b1729235aad
santhosh-tarento <santhosh.kumar@tarento.com>
2024-01-07 04:12:47 -0500

    new version of test cases updated

--- projects/sb-taxonomy-editor/src/lib/services/framework.service.spec.ts
+++ projects/sb-taxonomy-editor/src/lib/services/framework.service.spec.ts
@@ -291,7 +291,10 @@ describe('FrameworkService', () => {
 
 
 
-  let  rootConfig =  [
+  const  rootConfig = [
+        {
+        frameworkId:'tarento_fw',
+         config:[
             {   
                 "index": 1,
                 "category": "taxonomyCategory1",
@@ -322,8 +325,47 @@ describe('FrameworkService', () => {
                 "icon": "account_box",
                 "color": "#d8666a"
             }
-    ];
+        ]
+        },
+        {
+                "frameworkId" :"default",
+                "config" : [
+                    {   
+                        "index": 1,
+                        "category": "taxonomyCategory1",
+                        "icon": "settings",
+                        "color": "#1d2327"
+                    },
+                    {   
+                        "index": 2,
+                        "category": "taxonomyCategory2",
+                        "icon": "extension",
+                        "color": "#541675"
+                    },
+                    {   
+                        "index": 3,
+                        "category": "taxonomyCategory3",
+                        "icon": "bar_chart",
+                        "color": "#9a6c80"
+                    },
+                    {   
+                        "index": 4,
+                        "category": "taxonomyCategory4",
+                        "icon": "account_box",
+                        "color": "#d8666a"
+                    },
+                    {   
+                        "index": 5,
+                        "category": "taxonomyCategory5",
+                        "icon": "account_box",
+                        "color": "#d8666a"
+                    }
+        
+                ]
+            }
 
+    ]
+    
 
   let serviceRes = {
     "result": {
